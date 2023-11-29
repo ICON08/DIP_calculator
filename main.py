eb9b66ecebee46c5a6015f5282d945e44fe0c369
ICON08 <techatat777@gmail.com>
2023-11-29 11:15:43 +0700

    make new branch

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import tkinter as tk
 class MyCalculator:
     def __init__(self):
         self.root = tk.Tk()
-        self.root.geometry("1080x480")
+        self.root.geometry("480x600")
         self.root.title("Calculator Pro X +++")
 
-        self.label = tk.Label(self.root, text="Hello DIP01", font=('Ariel', 69))
-        self.label.pack()
+        self.label = tk.Label(self.root, text="0", font=('Ariel', 50))
+        self.place(x=500, y=150)
         self.button = tk.Button(self.root, text="Click here", font=('ariel', 10))
         self.button.place(x=500, y=150)
 
