43a712b8d3a567f9b15d51160f4ff9f876ce41ac
ICON08 <techatat777@gmail.com>
2023-11-29 10:54:10 +0700

    #3edit

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ class MyCalculator:
         self.root.geometry("1080x480")
         self.root.title("Calculator Pro X +++")
 
-        self.label = tk.Label(self.root, text="Calculator Pro X +++", font=('Comic Sans MS', 69))
+        self.label = tk.Label(self.root, text="Hello DIP01", font=('Ariel', 69))
         self.label.pack()
-        self.button = tk.Button(self.root, text="Download Here", font=('Comic Sans MS', 10))
+        self.button = tk.Button(self.root, text="Click here", font=('ariel', 10))
         self.button.place(x=500, y=150)
 
         self.root.mainloop()
